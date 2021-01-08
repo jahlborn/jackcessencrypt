bd1c7e105602854ef8888a797524eb3026e3ae9d
James Ahlborn <jtahlborn@yahoo.com>
2021-01-08 01:44:45 +0000

    relocate compat class to crypt sub-module
    git-svn-id: https://svn.code.sf.net/p/jackcessencrypt/code/branches/modules@160 dccba6bd-fee7-46c2-a6e6-2534e08c6e9c

--- src/compat/compile_legacy.sh
+++ src/compat/compile_legacy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMPAT_CLASSPATH="../../target/jackcess-encrypt-2.1.1-SNAPSHOT.jar:bcprov-jdk15-1.45.jar:jackcess-2.1.0.jar:commons-logging-1.1.1.jar:commons-lang-2.6.jar"
+COMPAT_CLASSPATH="../../target/jackcess-encrypt-3.0.1.mods-SNAPSHOT.jar:bcprov-jdk15-1.45.jar:jackcess-3.0.0.jar:commons-logging-1.1.1.jar:commons-lang3-3.10.jar"
 
 javac -cp $COMPAT_CLASSPATH com/healthmarketscience/jackcess/crypt/util/RC4EngineLegacy.java
 
